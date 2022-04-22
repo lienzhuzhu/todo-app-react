a0c93f5f6f4303ce44921f15a0951ff3bd66c515
Lien Zhu <lienzhucs@gmail.com>
2022-04-22 15:47:55 -0500

    Updates

--- precache-manifest.270bb44d9d6261d78570fb4410e9de60.js
+++ precache-manifest.270bb44d9d6261d78570fb4410e9de60.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "dad2063faf0ba6390fe7",
+    "revision": "79e2a09cffa042e68e3a",
     "url": "/09-todo-react-group-todo-lien/static/css/main.102cda08.chunk.css"
   },
   {
-    "revision": "dad2063faf0ba6390fe7",
-    "url": "/09-todo-react-group-todo-lien/static/js/main.dad2063f.chunk.js"
+    "revision": "79e2a09cffa042e68e3a",
+    "url": "/09-todo-react-group-todo-lien/static/js/main.79e2a09c.chunk.js"
   },
   {
     "revision": "03dd3806302b760fbd85",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/09-todo-react-group-todo-lien/static/js/runtime~main.8814a2a3.js"
   },
   {
-    "revision": "4cafbfcd3679dc3bb6423c8d760d6c00",
+    "revision": "54deb5d9cc024fa3784ef7446f6f0b86",
     "url": "/09-todo-react-group-todo-lien/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/09-todo-react-group-todo-lien/precache-manifest.5e6da7eb6e5cbb33ceb5823810232d57.js"
+  "/09-todo-react-group-todo-lien/precache-manifest.270bb44d9d6261d78570fb4410e9de60.js"
 );
 
 workbox.clientsClaim();
