a995823e33441a942f97eb53225111fc0ccf3c26
Lien Zhu <lienzhucs@gmail.com>
2022-06-26 22:28:38 -0500

    Updates

---
+++ precache-manifest.236e1963bbe6a2b9ef951f7006a1c32b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22ef4c458d51d21c87c4",
+    "url": "/todo-app-react/static/css/main.897faa8a.chunk.css"
+  },
+  {
+    "revision": "22ef4c458d51d21c87c4",
+    "url": "/todo-app-react/static/js/main.22ef4c45.chunk.js"
+  },
+  {
+    "revision": "03dd3806302b760fbd85",
+    "url": "/todo-app-react/static/js/1.03dd3806.chunk.js"
+  },
+  {
+    "revision": "b4581c3cf0ca89a7dceb",
+    "url": "/todo-app-react/static/js/runtime~main.b4581c3c.js"
+  },
+  {
+    "revision": "d682bdefb2061808abd19d7eae6e6d5e",
+    "url": "/todo-app-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac79f6350270b101f2d79e6e1abc0f98.js"
+  "/todo-app-react/precache-manifest.236e1963bbe6a2b9ef951f7006a1c32b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/todo-app-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
